b805b2f42a806755b346b2fdb7d14adbf7028905
dasom222g <dasom228@gmail.com>
2025-04-09 17:08:37 +0900

    feat: 페이지 이동 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 import Greeting from "../components/Greeting";
-import Button from "../components/Button";
+// import Button from "../components/Button";
 import Counter from "../components/Counter";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  // logic
+  const history = useNavigate();
   const name = "후츠릿";
   const handleClick = () => {
     console.log("클릭");
+    history("/info"); // 이동할 페이지의 path
   };
 
+  // view
   return (
     <div>
-      Home
+      <h1>Home</h1>
       <Greeting userName={name} />
-      <Button text="버튼" onButtonClick={handleClick} />
+      {/* <Button text="버튼" onButtonClick={handleClick} /> */}
       <Counter />
-      <button type="button">Info페이지로 이동</button>
+      <button type="button" onClick={handleClick}>
+        Info페이지로 이동
+      </button>
     </div>
   );
 };
--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Info = () => {
+  // logic
+  const history = useNavigate();
+
+  // view
   return (
     <div>
-      Info
-      <button type="button">Home페이지로 이동</button>
+      <h1>Info</h1>
+      <button type="button" onClick={() => history("/")}>
+        Home페이지로 이동
+      </button>
     </div>
   );
 };
