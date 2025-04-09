c510cbe785ebd66610ff32f2c3022973b64b5ad1
dasom222g <dasom228@gmail.com>
2025-04-09 14:19:14 +0900

    refactoring

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,32 +3,26 @@ import React, { useState } from "react";
 
 const Counter = () => {
   // logic
-  // let count = 10;
 
   // 첫번째: 데이터, 두번째: 함수
   const [countS, setCountS] = useState(0);
 
-  const handleIncrease = () => {
-    console.log("click!!");
-    // count = count + 1
-    // count = count + 1;
-    // console.log("🚀 ~ handleIncrease ~ count:", count);
-    setCountS(countS + 1);
-  };
-
   // 미션
-  // -1버튼 클릭시 handleDecrease라는 함수를 호출하여 count값 감소시키기
-  const handleDecrease = () => {
-    setCountS((prev) => prev - 1);
-  };
+  // countS의 값 - 최소값: 0, 최대값 10
+  const MIN = 0;
+  const MAX = 10;
 
   const handleCount = (type) => {
     // type: increase | decrease
-    setCountS(type === "increase" ? countS + 1 : countS - 1);
+    // setCountS(type === "increase" ? countS + 1 : countS - 1);
+    if (type === "increase") {
+      // increase버튼 클릭시
+      countS < MAX && setCountS(countS + 1);
+    } else {
+      // decrease버튼 클릭시
+      countS > MIN && setCountS(countS - 1);
+    }
   };
-
-  // 미션
-  // countS의 값 - 최소값: 0, 최대값 10
 
   // view
   return (
